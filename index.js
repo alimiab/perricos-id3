ffa4d6676e38a3381ebd8522ff663ce09616582f
alimiab <aliyah@aliyahbate.com>
2026-01-08 15:39:47 +0100

    update file descriptions in CSS, HTML, and JavaScript files for clarity; add dog objects in JS

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-// file for main JavaScript functionality
-const perricosArray = [];
+// file for main JS code
+import { getRandomDogImage } from './api.js';
+
+const perricosArray = [Coco, Rocky, Luna, Lola, Daisy, Max]; // Imágenes de perros
+
+// Ejemplo de acceso a los elementos del array
+console.log(perricosArray[0]); // Coco
+console.log(perricosArray[1]); // Rocky
+console.log(perricosArray[2]); // Luna
+console.log(perricosArray[3]); // Lola
+console.log(perricosArray[4]); // Daisy
+console.log(perricosArray[5]); // Max
+
+// Ejemplo de objetos perro // Puedes expandir estos objetos según sea necesario
+//example of dog objects //you can expand these objects as needed
+const perro1 = {
+  id: 1,
+  name: "Coco",
+  votes: 0
+};
+const perro2 = {
+  id: 2,
+  name: "Rocky",
+  votes: 0
+}; 
+const perro3 = {
+  id: 3,
+  name: "Luna",
+  votes: 0
+};
+const perro4 = {
+  id: 4,
+  name: "Lola",
+  votes: 0
+};
+const perro5 = {
+  id: 5,
+  name: "Daisy",
+  votes: 0
+};
+const perro6 = {
+  id: 6,
+  name: "Max",
+  votes: 0
+};
+
+//gives a random name from the array and assigns it to randomName
+const randomName = names[Math.floor(Math.random() * names.length)];
 
 // addPerrico();
 
